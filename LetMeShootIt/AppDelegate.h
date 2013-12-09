85d3e320620d763b9e0694103a00fc93c9532ed9
user <user@usertekiMac-mini.local>
2013-12-09 09:54:32 +0800

    Initial Commit

---
+++ LetMeShootIt/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  LetMeShootIt
+//
+//  Created by user on 13-12-9.
+//  Copyright only 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ LetMeShootIt/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LetMeShootIt
+//
+//  Created by user on 13-12-9.
+//  Copyright only 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
